344e448722870739604693969a9b34d321c511a9
Naved Rasul <navedrasul@gmail.com>
2020-12-17 22:35:07 +0500

    Added a project to convert C# model classes to TypeScript
    ... in a single file (generated in bin/debug folder, named api-models.ts). Copy it to the /src/app/api-models folder in TrakGudUI project to update the models in the front-end project.

---
+++ TrakGud.TSMGen/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using TrakGud.DAL.Models;
+using TypeScriptBuilder;
+
+namespace TrakGud.TSMGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Converting C# Models to TypeScript Models:\n");
+
+            var ts = new TypeScriptGenerator();
+
+            Type CSharpModel__type = typeof(CmAddress);
+            Console.WriteLine("Converting " + CSharpModel__type.Name + " to TypeScript Model in file api-models.ts");
+
+            // add types you need (dependant types will be added automatically)
+            ts.AddCSType(CSharpModel__type);
+
+            // get generated code as string
+            ts.ToString();
+
+            // or write to file
+            ts.Store("api-models.ts");
+        }
+    }
+}
